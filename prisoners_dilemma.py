5e2a0f7f8e06a10d21a309caeaebff63dc1853b2
Tony Graham <graham.fbhs@gmail.com>
2015-06-17 16:46:02 -0700

    Update prisoners_dilemma.py

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 PrisonerDilemma.py allows hard-coding different strategies
 for the Iterative Prisoners Dilemma, the canonical game of game-theory.
 Each strategy plays 100 to 200 rounds against each other strategy.
-The results of all previous rounds within a 100-200 round stretch are known
+Results of all previous rounds within a 100-200 round stretch are known
 to both players. 
 
 play_tournament() executes the tournament and stores output in tournament.txt
